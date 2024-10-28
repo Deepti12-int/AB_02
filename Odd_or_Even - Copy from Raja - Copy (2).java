86aa8f173351d7a4118dbffe45b47126517305a4
Rajaraman Ganeshan <g.rajaraman93@gmail.com>
2024-10-28 22:39:49 +0530

    new change - addition of files

---
+++ Odd_or_Even - Copy from Raja - Copy (2).java
@@ -0,0 +1,36 @@
+package pt_Deepti;
+
+public class Odd_or_Even
+{
+	public static void main(String[] args)
+	{
+		int a =1000;
+		if (a%2==0)
+		{
+			System.out.println("Even Number");			
+		}
+		add();
+		Odd_or_Even S= new Odd_or_Even();
+	S.sub();
+	}
+	
+	static void add()
+	{
+		int a=25;
+		int b=45;
+		int c=a+b;
+		System.out.println("the sum of a+b="+ c);
+		
+	}
+void sub()
+	{
+		int a=25;
+		int b=45;
+		int c=a-b;
+		System.out.println("the difference of a-b="+ c);
+		
+	}
+
+
+}
+//Hello this is me.
